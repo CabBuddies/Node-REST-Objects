8f293e21ee79ca85fdda9d4f12d75543d239553d
Nihal Konda <nihal.konda@sjsu.edu>
2020-10-19 15:36:24 -0700

    [Fixed] API

--- dist/rest/api.d.ts
+++ dist/rest/api.d.ts
@@ -3,33 +3,6 @@ declare const DOMAIN: {
     QUERIES: string;
     GROUPS: string;
 };
-declare const API: {
-    USER_MANAGEMENT: {
-        AUTH: {
-            BASE: string;
-            SIGN_UP: string;
-            SIGN_IN: string;
-            ACCESS_TOKEN: string;
-            SIGN_OUT: string;
-            SIGN_OUT_ALL: string;
-        };
-        USER: {
-            BASE: string;
-            ME: string;
-        };
-    };
-    QUERIES: {
-        QUERY: string;
-        RESPONSE: string;
-        COMMENT: () => string;
-        OPINION: () => string;
-        TAG: string;
-    };
-    GROUPS: {
-        GROUP: string;
-        POST: string;
-        REPLY: string;
-        OPINION: string;
-    };
-};
-export { API, DOMAIN };
+declare let API: any;
+declare const refreshAPI: () => void;
+export { API, DOMAIN, refreshAPI };
